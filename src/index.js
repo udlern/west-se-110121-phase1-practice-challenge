2f314c9d89c6cb7ae5814e063a00c83562a008b2
Noa Udler <udlernoa4@gmail.com>
2021-11-10 11:08:52 -0800

    what i have so far

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-// Your code here
+const generateElement = element => document.createElement(element)
+function renderCharacterName(name) {
+    const span = generateElement('span')
+    const characterDiv = document.querySelector("#character-bar")
+    characterDiv.appendChild(span)
+    const characterName = name 
+    span.appendChild(characterName)
+}
+
+function init() {
+    characters.forEach(renderCharacterName);
+}
+
+init();
